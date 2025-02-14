1896eb8d4c124adb0e7d4729ebd11035dbabdd4d
Chen lin tao <2572886630@qq.com>
2025-02-14 15:27:47 +0800

    .

---
+++ Red Cross-java/Red Cross-java/src/main/java/com/example/redcross/model/User.java
@@ -0,0 +1,86 @@
+package com.example.redcross.model;
+
+public class User {
+
+    private int user_id;
+    private String user_name;
+    private String picture_url;
+    private String email;
+    private String account;
+    private String password;
+    private int user_type;
+
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getPicture_url() {
+        return picture_url;
+    }
+
+    public void setPicture_url(String picture_url) {
+        this.picture_url = picture_url;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getUser_type() {
+        return user_type;
+    }
+
+    public void setUser_type(int user_type) {
+        this.user_type = user_type;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "user_id=" + user_id +
+                ", user_name='" + user_name + '\'' +
+                ", picture_url='" + picture_url + '\'' +
+                ", email='" + email + '\'' +
+                ", account='" + account + '\'' +
+                ", password='" + password + '\'' +
+                ", user_type=" + user_type +
+                '}';
+    }
+
+
+
+
+}
---
+++ Red Cross-java/Red Cross-java/src/main/java/com/example/redcrossjava/RedCrossJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.redcrossjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedCrossJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedCrossJavaApplication.class, args);
+    }
+
+}
